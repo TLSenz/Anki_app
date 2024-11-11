21a3975546fe31f1b49c26b93b0a8d1598956503
TLSenz <sven@zemp.email>
2024-11-11 14:25:30 +0100

    work on Anki Project

--- functions.py
+++ functions.py
@@ -1,18 +1,22 @@
-def system_start():
-    pass
+import sqlite3
 
 
-
-
-def get_card():
-    pass
-
+def get_cardarray(cardnumber):
+    conn = sqlite3.connect('Anki.db')
+    cursor = conn.cursor()
+    get_card = """ SELECT * FROM Flashcards WHERE ID = ?"""
+    cursor.execute(get_card, (cardnumber, ))
+    card = cursor.fetchone()
+    return card
 
 def cal_score():
     pass
 
-def return_card():
-    pass
+def return_card(DaysRevesion,Date,):
+    conn = sqlite3.connect('Anki.db')
+    cursor = conn.cursor()
+    sql_returncard = """Insert into Flashcards (DaysRevesion) VALUES (?)""","""Insert into Flashcards (Date) VALUES (?)"""
+    cursor.execute(sql_returncard, (DaysRevesion,Date))
 
 
 
